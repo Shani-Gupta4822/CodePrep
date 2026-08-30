f7f8d89fb85a457a2b9367a2854c34e348bb2d07
Shani Gupta <shanigupta90058@gmail.com>
2026-08-31 00:30:18 +0530

    Configure PostgreSQL for deployment

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -3,7 +3,7 @@ Django settings for config project.
 """
 
 import os
-
+import dj_database_url
 from dotenv import load_dotenv
 from pathlib import Path
 
@@ -128,40 +128,46 @@ TEMPLATES = [
 # DATABASE
 # ============================================================
 
-DATABASES = {
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-    "default": {
-
-        "ENGINE": "django.db.backends.mysql",
-
-        "NAME": os.getenv(
-            "DB_NAME",
-            "codeprep"
-        ),
-
-        "USER": os.getenv(
-            "DB_USER",
-            "root"
-        ),
-
-        "PASSWORD": os.getenv(
-            "DB_PASSWORD",
-            ""
-        ),
-
-        "HOST": os.getenv(
-            "DB_HOST",
-            "localhost"
-        ),
-
-        "PORT": os.getenv(
-            "DB_PORT",
-            "3306"
-        ),
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600
+        )
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.mysql",
 
+            "NAME": os.getenv(
+                "DB_NAME",
+                "codeprep"
+            ),
 
+            "USER": os.getenv(
+                "DB_USER",
+                "root"
+            ),
+
+            "PASSWORD": os.getenv(
+                "DB_PASSWORD",
+                ""
+            ),
+
+            "HOST": os.getenv(
+                "DB_HOST",
+                "localhost"
+            ),
+
+            "PORT": os.getenv(
+                "DB_PORT",
+                "3306"
+            ),
+        }
+    }
 # ============================================================
 # PASSWORD VALIDATION
 # ============================================================
